adaa1c3edb48efb829acac8263bf41a7039f47a2
tbfly <tombowfly@gmail.com>
2017-03-10 14:31:06 +0800

    *): Adjust for coding style.

--- run.sh
+++ run.sh
@@ -4,5 +4,6 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --time=01:00:00
 
-/usr/lib64/mpich/bin/mpirun -np 2 ./ItemCF 0 10 1 2 3 4
+#/usr/lib64/mpich/bin/mpirun -np 2 ./ItemCF 0 10 1 2 3 4
+/usr/lib64/mpich/bin/mpirun -np 2 ./ItemCF 1 10 1 2 3 4
 
